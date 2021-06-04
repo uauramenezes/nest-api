c0c808f9034f1b68baa8104859f500978f039c87
uauramenezes <uaurasilva@hotmail.com>
2021-06-04 14:45:23 -0300

    Add hashing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,18 @@
+import isStrongPassword from 'validator/lib/isStrongPassword';
+import isEmail from 'validator/lib/isEmail';
+import isAlpha from 'validator/lib/isAlpha';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 import {
   Injectable,
   ForbiddenException,
   BadRequestException,
   NotFoundException,
 } from '@nestjs/common';
-import isEmail from 'validator/lib/isEmail';
-import isAlpha from 'validator/lib/isAlpha';
-import isStrongPassword from 'validator/lib/isStrongPassword';
 
 import { User } from 'src/users/users.entity';
 import { UsersService } from 'src/users/users.service';
+import { UserDto } from 'src/users/dto/user.dto';
 
 @Injectable()
 export class AuthService {
@@ -19,9 +21,11 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async createUser(reqUser: User) {
-    const result = await this.validateData(reqUser);
+  async createUser(reqUser: UserDto) {
+    const result = await this.validateUserData(reqUser);
     if (!result) return result;
+
+    reqUser.password = await bcrypt.hash(reqUser.password, 10);
 
     const newUser = await this.usersService.create(reqUser);
     const payload = {
@@ -33,26 +37,6 @@ export class AuthService {
     return {
       access_token: this.jwtService.sign(payload),
     };
-  }
-
-  async validateData(user: User) {
-    const result = await this.usersService.findOne(user.email);
-    if (result) throw new ForbiddenException('Email already in use!');
-
-    if (!isAlpha(user.name)) throw new BadRequestException('Invalid name');
-    if (!isEmail(user.email)) throw new BadRequestException('Invalid email');
-    if (!isStrongPassword(user.password))
-      throw new BadRequestException('Weak password!');
-
-    return true;
-  }
-
-  async validateUser(email: string, password: string): Promise<any> {
-    const user = await this.usersService.findOne(email);
-
-    if (user && user.password === password) return user;
-    if (!user) return { error: 'User not found!' };
-    return { error: 'Invalid password' };
   }
 
   async login(user: User) {
@@ -71,5 +55,26 @@ export class AuthService {
     const result = await this.usersService.delete(user.id);
     if (result.affected > 0) return { ok: 'Done' };
     throw new NotFoundException();
+  }
+
+  async validateUserData(user: UserDto) {
+    const result = await this.usersService.findOne(user.email);
+    if (result) throw new ForbiddenException('Email already in use!');
+
+    if (!isAlpha(user.name)) throw new BadRequestException('Invalid name');
+    if (!isEmail(user.email)) throw new BadRequestException('Invalid email');
+    if (!isStrongPassword(user.password))
+      throw new BadRequestException('Weak password!');
+
+    return true;
+  }
+
+  async validateUser(email: string, password: string): Promise<User | number> {
+    const user = await this.usersService.findOne(email);
+    const match = await bcrypt.compare(password, user.password);
+
+    if (user && match) return user;
+    if (!user) return 404;
+    return 401;
   }
 }
--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,7 +1,11 @@
 import { Strategy } from 'passport-local';
-import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from '../auth.service';
+import { PassportStrategy } from '@nestjs/passport';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -14,7 +18,9 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
 
   async validate(username: string, password: string): Promise<any> {
     const result = await this.authService.validateUser(username, password);
-    if (result.error) throw new UnauthorizedException(result.error);
+
+    if (result === 404) throw new NotFoundException();
+    if (result === 401) throw new UnauthorizedException();
 
     return result;
   }
